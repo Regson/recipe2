852d6639844f2ae83b0b33fb106229f4a2af961a
unknown <regha87@gmail.com>
2020-07-28 00:19:43 +0100

    Commandline recipe app

---
+++ app.py
@@ -0,0 +1,100 @@
+import database as db
+
+menu = """Please Select One of the following Options:
+1. Add Recipes
+2. Add Ingredients
+3. View all Recipes
+4. View all Ingredients
+5. Add Ingredient(s) to Recipe
+6. View a Recipe & its Ingredients
+7. Exit
+
+your selection: """
+
+welcome = """
+    ---------------------------------
+    |   WELCOME TO MY RECIPE APP    |
+    ---------------------------------
+"""
+print(welcome)
+db.create_tables()
+
+
+def add_recipe():
+    recipe_name = input("Enter recipe name: ")
+    db.add_recipe(recipe_name)
+
+
+def add_ingredients():
+    ingredient_name = input("Enter ingredient name: ")
+    db.add_ingredient(ingredient_name)
+
+
+def add_recipe_ing():
+    recipe_name = input("Recipe name: ")
+    ingredient_name = input("Ingredient name: ")
+    
+    recipeName = db.find_recipe_ing_name(ingredient_name)
+    if recipe_name in str(recipeName):
+        print(str(recipeName))
+        print(f"\n<-- {recipe_name} already has {ingredient_name} -->\n")
+    else:
+        db.add_recipe_ing(recipe_name, ingredient_name)
+        print("\n<-- Data saved successfully -->\n")
+        
+
+def print_all_recipe(header, recipes):
+    print(f"<-- {header} Recipes -->")
+    _id = 1
+    for recipe, in recipes:
+        print(f"{_id}. {recipe}")
+        _id += 1
+
+
+def print_all_ingredients(header, ingredients):
+    print(f"<-- {header} Ingredients -->")
+    _id = 1
+    for ingr, in ingredients:
+        print(f"{_id}. {ingr}")
+        _id += 1
+
+
+def print_all_recipe_ing():
+    name = input("Recipe name: ")
+    ingredients = db.get_recipe_ing(name)
+    if ingredients:
+        print_all_ingredients(name, ingredients)
+    else:
+        print("No ingredients")
+    print("------------\n")
+
+while (user_input := input(menu)) != "7":
+    if user_input == "1":
+        try:
+            add_recipe()
+        except Exception as e:
+            print(e, "::Error: recipe already exist.::")
+    elif user_input == "2":
+        try:
+            add_ingredients()
+        except Exception as e:
+            print(e, "::Error: ingredient already exist.::")
+    elif user_input == "3":
+        recipes = db.get_all_recipe()
+        if recipes:
+            print_all_recipe("All", recipes)
+        else:
+            print("No recipe found!")
+        print("------------\n")
+    elif user_input == "4":
+        ingr = db.get_all_ingredients()
+        if ingr:
+            print_all_ingredients("All", ingr)
+        else:
+            print("No ingredient found!")
+        print("------------\n")
+    elif user_input == "5":
+        add_recipe_ing()
+    elif user_input == "6":
+        print_all_recipe_ing()
+    
---
+++ database.py
@@ -0,0 +1,84 @@
+import sqlite3
+
+CREATE_TABLE_RECIPE = """CREATE TABLE IF NOT EXISTS recipe (
+    name TEXT PRIMARY KEY
+)"""
+CREATE_TABLE_INGREDIENTS = """CREATE TABLE IF NOT EXISTS ingredients (
+    name TEXT PRIMARY KEY
+)"""
+CREATE_TABLE_RECIPE_ING = """CREATE TABLE IF NOT EXISTS recipe_ing (
+    recipe_name TEXT,
+    ingredient_name TEXT,
+    FOREIGN KEY (recipe_name) REFERENCES recipe(name),
+    FOREIGN KEY (ingredient_name) REFERENCES ingredients(name)
+)"""
+
+ADD_RECIPE = "INSERT INTO recipe VALUES(?)"
+ADD_INGREDIENT = "INSERT INTO ingredients VALUES(?)"
+ADD_RECIPE_ING = "INSERT INTO recipe_ing VALUES(?, ?)"
+VIEW_ALL_RECIPE = "SELECT * FROM recipe"
+VIEW_ALL_INGREDIENTS = "SELECT * FROM ingredients"
+VIEW_RECIPE_ING = """SELECT ingredients.*
+    FROM ingredients
+    JOIN recipe_ing ON recipe_ing.ingredient_name = ingredients.name
+    JOIN recipe ON recipe.name = recipe_ing.recipe_name
+    WHERE recipe.name = ?;"""
+RECIPE_BY_NAME = "SELECT * FROM recipe WHERE recipe.name = ?;"
+RECIPE_ING_BY_NAME = "SELECT recipe_name FROM recipe_ing WHERE recipe_ing.ingredient_name = ?;"
+
+connection = sqlite3.connect("recipeDB.db")
+
+def create_tables():
+    with connection:
+        connection.execute(CREATE_TABLE_RECIPE)
+        connection.execute(CREATE_TABLE_INGREDIENTS)
+        connection.execute(CREATE_TABLE_RECIPE_ING)
+
+
+def find_recipe_by_name(name):
+    with connection:
+        cursor = connection.cursor()
+        cursor.execute(RECIPE_BY_NAME, (name,))
+        return cursor.fetchone()
+
+
+def find_recipe_ing_name(name):
+    with connection:
+        cursor = connection.cursor()
+        cursor.execute(RECIPE_ING_BY_NAME, (name,))
+        return cursor.fetchall()
+
+
+def add_recipe(name):
+    with connection:
+        connection.execute(ADD_RECIPE, (name,))
+
+
+def add_ingredient(name):
+    with connection:
+        connection.execute(ADD_INGREDIENT, (name,))
+
+
+def add_recipe_ing(recipe_name, ingredient_name):
+    with connection:
+        connection.execute(ADD_RECIPE_ING, (recipe_name, ingredient_name))
+
+
+def get_all_recipe():
+    with connection:
+        cursor = connection.cursor()
+        cursor.execute(VIEW_ALL_RECIPE)
+        return cursor.fetchall()
+
+
+def get_all_ingredients():
+    with connection:
+        cursor = connection.cursor()
+        cursor.execute(VIEW_ALL_INGREDIENTS)
+        return cursor.fetchall()
+
+def get_recipe_ing(recipeName):
+    with connection:
+        cursor = connection.cursor()
+        cursor.execute(VIEW_RECIPE_ING, (recipeName,))
+        return cursor.fetchall()
